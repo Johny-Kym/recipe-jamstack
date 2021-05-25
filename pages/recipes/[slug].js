221576a1010495d46fa12217b38c685df854cf7c
johny kym <johny@localhost.localdomain>
2021-05-25 12:02:59 +0300

    bug

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -37,10 +37,11 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function RecipeDetails({ recipe }) {
+  if(!recipe) return <div>Loading...</div>
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
 
-    if(!recipe) <div>Loading...</div>
+   
 
   return (
       <>
