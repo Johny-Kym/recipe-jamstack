7f4ffd55801e3e4226ed6647ec4c05a67a7b0493
johny kym <johny@localhost.localdomain>
2021-05-25 13:52:14 +0300

    arrow CTA

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
 
 export default function Recipe({ recipe }) {
   const { title, slug, cookingTime, thumbnail } = recipe.fields;
@@ -27,8 +28,8 @@ export default function Recipe({ recipe }) {
         {/* CTA buttons*/}
         <div className="mt-10 flex justify-end actions">
           <Link  href={`recipes/${slug}`} >
-              <button style={{background:"#2460FB"}} className=" hover:text-black  py-2 px-4 text-white">
-                  Cook now
+              <button  className="  bg-blue-700 hover:bg-green-600 font-bold  transition-all transform  hover:-rotate-2  py-2 px-4 text-white">
+                  Cook now <TrendingFlatIcon/>
               </button>
           </Link>
         </div>
