0df56110e4bc1b4135dafb4b2d0c56b98a15e6ce
johny kym <johny@localhost.localdomain>
2021-05-25 12:15:21 +0300

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createClient } from "contentful";
 import Image from "next/image";
-import { documentToReactComponents}  from '@contentful/rich-text-react-renderer'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Header from "../../components/Header";
 const client = createClient({
   space: "wumj2bwqym59",
@@ -32,45 +32,45 @@ export const getStaticProps = async ({ params }) => {
     props: {
       recipe: items[0],
     },
-    revalidate:1
+    revalidate: 1,
   };
 };
 
 export default function RecipeDetails({ recipe }) {
-  if(!recipe) return <div>Loading...</div>
+  if (!recipe) return <div>Loading...</div>;
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
 
-   
-
   return (
-      <>
-      <Header/>
-    <div className="px-2 md:px-10 lg:px-36 ">
-      {/*Banner */}
-      <div className="mt-10">
-        <Image
-          src={`https:${featuredImage.fields.file.url}`}
-          width={featuredImage.fields.file.details.image.width}
-          height="2000"
-        />
-        <h1 className="bg-white shadow-lg py-3 w-max font-black text-xl uppercase relative -top-10 -left-1 transform -rotate-1">{title}</h1>
-      </div>
-      {/*info */}
-      <p className="mt-0 mb-2">Take about {cookingTime} mins</p>
-      <h3 className=" font-black text-xl">Ingredients</h3>
+    <>
+      <Header />
+      <div className="px-2 md:px-10 lg:px-36 ">
+        {/*Banner */}
+        <div className="mt-10">
+          <Image
+            src={`https:${featuredImage.fields.file.url}`}
+            width={featuredImage.fields.file.details.image.width}
+            height="2000"
+          />
+          <h1 className="bg-white shadow-lg py-3 w-max font-black text-xl uppercase relative -top-10 -left-1 transform -rotate-1">
+            {title}
+          </h1>
+        </div>
+        {/*info */}
+        <p className="mt-0 mb-2">Take about {cookingTime} mins</p>
+        <h3 className=" font-black text-xl">Ingredients</h3>
 
-      <ol className="mb-4">
-      {ingredients.map((ing) => (
-        <li key={ing}>-{ing}</li>
-      ))}
-      </ol>
-     
-      <div className="">
+        <ol className="mb-4">
+          {ingredients.map((ing) => (
+            <li key={ing}>-{ing}</li>
+          ))}
+        </ol>
+
+        <div className="">
           <h1 className="uppercase font-black text-xl ">Method</h1>
           <div>{documentToReactComponents(method)}</div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
